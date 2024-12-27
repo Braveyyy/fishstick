8e2259cc867d2b6dde5c14b7b8d7774b9d36ca48
Adam <latifasedyeame@gmail.com>
2024-12-27 15:18:29 -0700

    Create LandingPage.js

--- frontend/fit-react/src/App.js
+++ frontend/fit-react/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './LandingPage.js';
+import LandingPage from './LandingPage.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ frontend/fit-react/src/LandingPage.js
@@ -0,0 +1,8 @@
+
+export default function LandingPage() {
+    return(
+        <div className = "LandingPage">
+            <h1>Fishstick</h1>
+        </div>
+    );
+}
